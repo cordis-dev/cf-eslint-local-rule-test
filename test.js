45721e499e189f5453e6ef2b5b6fed3a4f31eef1
cordis-dev <darius@adroiti.com>
2024-12-03 13:21:17 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-// test.js
-var foo = 'bar'; // This should trigger the custom rule
-let baz = 'qux'; // This should not trigger the rule
+function greet(name) {
+  console.log('Hello, ' + name);
+}
